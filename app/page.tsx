9698852cf86b03b55f370b2d6ae7bf6345ba7a72
Graff Alexandre <alexgraff67@gmail.com>
2024-09-11 09:27:39 +0200

    correction bug page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import Board from './components/Board';
 
 export default function Home() {
 	return (
-		<>
+		<div
 			className="min-h-screen flex flex-col justify-center items-center
-			bg-gray-100"
+				bg-gray-100"
+		>
 			<Board />
-		</>
+		</div>
 	);
 }
